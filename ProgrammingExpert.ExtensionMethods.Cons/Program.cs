1abe1004bbacd27ae06d97dbc09fca00e124b564
Dries Deboosere <dries.deboosere@howest.be>
2020-05-05 22:17:01 +0200

    Finish exercise

--- ProgrammingExpert.ExtensionMethods.Cons/Program.cs
+++ ProgrammingExpert.ExtensionMethods.Cons/Program.cs
@@ -8,7 +8,7 @@ namespace ProgrammingExpert.ExtensionMethods.Cons
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string text = "This is a line of text";
 
             // Plaats hier je code
 
